b953a2235e721373930bfdbd918dcc4b5a536d76
Piotr Misiuk <pmisiuk@gmail.com>
2018-10-26 20:16:56 +0200

    Binary to decimal add shift

--- binary-decimal/sketch.js
+++ binary-decimal/sketch.js
@@ -1,5 +1,7 @@
 let num = '';
 let byte = [];
+let shiftLeft;
+let shiftRight;
 let decimalP;
 
 function mousePressed() {
@@ -9,27 +11,55 @@ function mousePressed() {
 }
 
 function setup() {
-  createCanvas(400, 100);
+	createCanvas(400, 100);
+	frameRate(12);
 	binaryToDecimal(num);
 	decimalP = createP('');
+	shiftLeft = createButton('<<');
+	shiftRight = createButton('>>');
+	shiftLeft.mousePressed(shiftBitsLeft);
+	shiftRight.mousePressed(shiftBitsRight);
 
 	let w = width/8;
 	for (let i=0; i<8; i++) {
 		byte[i] = new Bit(w/2 + i * w, 50, w - 4);
 		byte[i].setState(num.charAt(i));
 	}
-
 }
 
-function draw() {
-	background(70, 130, 180);
-	num = '';
+function shiftBitsLeft() {
+	let num = getBinaryString();
+	let val = binaryToDecimal(num);
+	val = val << 1;
+	num = decimalToBinary(val);
+	for (let i=0; i<8; i++) {
+		byte[i].setState(num.charAt(i));
+	}
+}
 
+function shiftBitsRight() {
+	let num = getBinaryString();
+	let val = binaryToDecimal(num);
+	val = val >> 1;
+	num = decimalToBinary(val);
+	for (let i=0; i<8; i++) {
+		byte[i].setState(num.charAt(i));
+	}
+}
+
+function getBinaryString() {
+	num = '';
 	for (let i=7; i>-1; i--) {
 		byte[i].show();
 		num += byte[i].state ? '1' : '0';
 	}
-	decimalP.html(binaryToDecimal(num))
+	return num;
+}
+
+function draw() {
+	background(70, 130, 180);
+
+	decimalP.html(binaryToDecimal(getBinaryString()));
 }
 
 function binaryToDecimal(val) {
@@ -39,4 +69,17 @@ function binaryToDecimal(val) {
 		sum += parseInt(bit * pow(2, i));
 	}
 	return sum;
+}
+
+function decimalToBinary(val) {
+	let bits = '';
+	let rem = val;
+
+	for (let i = 7; i >= 0; i--) {
+		let div = pow(2, i);
+		let bitVal = floor(rem / div);
+		bits += bitVal;
+		rem = rem % div;
+	}
+	return bits;
 }
