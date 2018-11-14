853c07b3a7aff828ff37a00f0ed685dd4f68b2a7
Piotr Misiuk <pmisiuk@gmail.com>
2018-11-14 13:51:08 +0100

    Fix urls in published sites

---
+++ meetup/precache-manifest.2970f7c00d7d72fa0a49004791451a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "603f734fbdd4e174e539",
+    "url": "js/chunk-vendors.da79d3bf.js"
+  },
+  {
+    "revision": "5f0d27814203b60fb526",
+    "url": "js/app.6080e90e.js"
+  },
+  {
+    "revision": "315e6487671746921bb342ce6b3d2bb0",
+    "url": "index.html"
+  },
+  {
+    "revision": "603f734fbdd4e174e539",
+    "url": "css/chunk-vendors.be7af032.css"
+  },
+  {
+    "revision": "5f0d27814203b60fb526",
+    "url": "css/app.b7756dc1.css"
+  }
+];
--- meetup/service-worker.js
+++ meetup/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41ccdeb437e2b7b3ed6c8e6fb426f0ea.js"
+  "precache-manifest.2970f7c00d7d72fa0a49004791451a58.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "meetup"});
